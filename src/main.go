dda61cca14e9e265fa150d16d8abbab1181a98f2
NiHaoyin <nhy716529@163.com>
2023-08-04 10:01:53 -0500

    updated on 08.04

--- src/main.go
+++ src/main.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	mapping := make(map[int][]int)
-	slice := make([]int, 10)
-	mapping[0] = slice
-	slice[0] = 9
-	//slice = append(slice, 100)
-	//mapping[0] = slice
-	fmt.Println(mapping)
+type MessageQueue struct {
+	queue chan string
+}
 
+func NewMessageQueue(size int) *MessageQueue {
+	return &MessageQueue{
+		queue: make(chan string, size),
+	}
+}
+
+func (mq *MessageQueue) Enqueue(message string) {
+	mq.queue <- message
+}
+
+func (mq *MessageQueue) Dequeue() string {
+	return <-mq.queue
+}
+
+func main() {
+	messageQueue := NewMessageQueue(3)
+
+	// Producer goroutine
+	go func() {
+		for i := 1; i <= 5; i++ {
+			message := fmt.Sprintf("Message %d", i)
+			messageQueue.Enqueue(message)
+			fmt.Printf("Produced: %s\n", message)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	// Consumer goroutine
+	go func() {
+		for i := 1; i <= 5; i++ {
+			message := messageQueue.Dequeue()
+			fmt.Printf("Consumed: %s\n", message)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Wait for goroutines to finish
+	time.Sleep(10 * time.Second)
 }
--- src/service/order_service.go
+++ src/service/order_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"Order-Scheduling-System/src/dao"
 	"errors"
+	"fmt"
 	"strconv"
 )
 
@@ -75,4 +76,47 @@ func CompleteOrder(orderID int) error {
 		return errors.New("OrderID not exist:" + strconv.Itoa(orderID))
 	}
 	return nil
+}
+
+func CancelOrder(orderID int) error {
+	orderExisted := false
+	for _, order := range dao.OrderDatabaseInstance.OrdersByStatus[dao.Placed] {
+		if orderID == order.ID {
+			orderExisted = true
+			err := dao.OrderDatabaseInstance.UpdateStatus(order, dao.Cancelled)
+			if err != nil {
+				return err
+			}
+			order.Shipment.StockCount++
+			return nil
+		}
+	}
+	for _, order := range dao.OrderDatabaseInstance.OrdersByStatus[dao.Gathering] {
+		if orderID == order.ID {
+			orderExisted = true
+			err := dao.OrderDatabaseInstance.UpdateStatus(order, dao.Cancelled)
+			if err != nil {
+				return err
+			}
+			order.Shipment.StockCount++
+			return nil
+		}
+	}
+	for _, order := range dao.OrderDatabaseInstance.OrdersByStatus[dao.OutForDelivery] {
+		if orderID == order.ID {
+			orderExisted = true
+			return errors.New(fmt.Sprintf("Order ID: %d is OutForDelivery. Cannot cancel it.", orderID))
+		}
+	}
+	for _, order := range dao.OrderDatabaseInstance.OrdersByStatus[dao.Delivered] {
+		if orderID == order.ID {
+			orderExisted = true
+			return errors.New(fmt.Sprintf("Order ID: %d is OutForDelivery. Cannot cancel it.", orderID))
+		}
+	}
+	if !orderExisted {
+		return errors.New(fmt.Sprintf("Order ID: %d doesn't exist.", orderID))
+	} else {
+		return nil
+	}
 }
